517b9799df34601b291bb7b438e77da6674287fe
Anifibous <Anifibous@DESKTOP-AFBL9I2>
2024-10-02 12:10:29 +0700

    Revert "Реализация линейной n-мерной функции"
    This reverts commit 87e43e5d9731659c27384ec434176cab974bffeb.

--- TOOP/Program.cs
+++ TOOP/Program.cs
@@ -1,9 +1,2 @@
-﻿using Functionals;
-using Functions;
-
-class Program
-{
-    static void Main()
-    {
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
