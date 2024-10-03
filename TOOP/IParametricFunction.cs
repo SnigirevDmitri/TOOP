556ffb83d2268da6916d1a29d4417a68f9cc2df8
Anifibous <Anifibous@DESKTOP-AFBL9I2>
2024-10-03 11:13:38 +0700

    Реализация полинома n-ой степени

--- TOOP/IParametricFunction.cs
+++ TOOP/IParametricFunction.cs
@@ -11,7 +11,7 @@ namespace Functions
         IFunction Bind(IVector parameters);
     }
 
-    // Функция имеет вид y = a0 + a1 * x1 + a2 * x2 + ... + an * xn
+    // Функция имеет вид y = a_0 + a_1 * x_1 + a_2 * x_2 + ... + a_n * x_n
     class LineFunction : IParametricFunction
     {
         private IVector _parameters;
@@ -61,4 +61,42 @@ namespace Functions
         }
     }
 
+    // Функция имеет вид y = a_0 + a_1 * x + a_2 * x^2 + ... + a_n * x^n
+    // Массив параметров выглядит следующим образом: [n, a_0, a_1, ..., a_n]
+    class PolynomFunction : IParametricFunction
+    {
+        private IVector _parameters;
+
+        public IFunction Bind(IVector parameters)
+        {
+            _parameters = parameters;
+            return new InternalPolynomFunction(_parameters);
+        }
+
+        class InternalPolynomFunction : IFunction
+        {
+            private readonly IVector _parameters;
+
+            public InternalPolynomFunction(IVector parameters)
+            {
+                _parameters = parameters;
+            }
+
+            public double Value(IVector point)
+            {
+                if (point.Count != 1)
+                    throw new ArgumentException("Размерность точки должна равна 1.");
+
+                if (_parameters.Count != _parameters[0] + 1)
+                    throw new ArgumentException("Размерность параметров должна быть равна n + 1.");
+
+                double res = _parameters[1];
+
+                for(int i = 2; i < _parameters.Count; i++)
+                    res += _parameters[i] * Math.Pow(point[0], i-1);
+
+                return res;
+            }
+        }
+    }
 }
--- TOOP/Program.cs
+++ TOOP/Program.cs
@@ -1,10 +1,28 @@
-﻿using Functionals;
+﻿using System;
 using Functions;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        // Создаем вектор параметров [n, a0, a1] для функции f(x) = 3 + 5 * x
+        IVector parameters = new Vector(new double[] { 3, 3, 5 });
 
+        // Инициализация линейной функции
+        IParametricFunction polynomFunc = new PolynomFunction();
+
+        // Привязка параметров (получаем конкретную функцию)
+        var boundFunc = polynomFunc.Bind(parameters);
+
+        // Точки для вычисления значения и градиента
+        IVector point1 = new Vector(new double[] { 4, 2 });
+        IVector point2 = new Vector(new double[] { -3 });
+        IVector point3 = new Vector(new double[] { 10 });
+
+        // Вычисление значений функции в этих точках
+        Console.WriteLine($"Значение функции в точке 4: {boundFunc.Value(point1)}");
+        Console.WriteLine($"Значение функции в точке -3: {boundFunc.Value(point2)}");
+        Console.WriteLine($"Значение функции в точке 10: {boundFunc.Value(point3)}");
     }
 }
+
