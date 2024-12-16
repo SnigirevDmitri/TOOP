9eea664dc1c6e6270efb55baed0216cc8768cce2
Александр <115202926+Tihonya237@users.noreply.github.com>
2024-12-16 12:48:09 +0700

    Доработка и исправление ошибок

---
+++ IMatrix.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public interface IMatrix : IList<IList<double>> { }
+public class Matrix : List<IList<double>>, IMatrix { }
