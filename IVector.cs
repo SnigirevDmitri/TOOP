a63e693f76fd49dc5287bf45cbffba5a48a6d807
Anifibous <Anifibous@DESKTOP-AFBL9I2>
2024-12-16 13:02:15 +0700

    Исправление

--- IVector.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public interface IVector : IList<double> { }
-public class Vector : List<double>, IVector { }
